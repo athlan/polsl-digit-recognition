a1d58130ae5dd342863ba80a7edb005710a2d08b
Athlan <athlan@vgroup.pl>
2014-03-20 22:13:31 +0100

    Display many images at once.

--- main.m
+++ main.m
@@ -5,17 +5,20 @@ labels = loadMNISTLabels('dataset/mnist/train-labels-idx1-ubyte');
 % we should transposite matrix to get images as rows instead of columns
 images = images'; 
 
+figure(1)
 % TEST
 % cut first row and translate it into matrix
-img = images(1:1,:);
-img = vec2mat(img, 28)';
+for j=1:10,
+    img = images(j:j,:);
+    img = vec2mat(img, 28)';
 
-% Add padding - +4 pixels each side to get 36x36 images
-% from 28x28 images
-img = padarray(img, [4 4]);
-
-% display image
-imshow(img);
+    % Add padding - +4 pixels each side to get 36x36 images
+    % from 28x28 images
+    img = padarray(img, [4 4]);
+    % display image
+    subplot(2,5,j)
+    imshow(img);
+end
 
 % display first 10 elements from label matrix
 disp(labels(1:10));
