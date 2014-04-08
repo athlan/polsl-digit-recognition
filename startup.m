5ff0407d634d2aa78fe144197f5c528115f4290e
Athlan <athlan@vgroup.pl>
2014-04-08 09:54:04 +0200

    Startup script added that sets Path at MatLab startup

---
+++ startup.m
@@ -0,0 +1,8 @@
+% This is startup script executed once when MatLab is launched
+
+% WARGNING! Please note that Startup location should be set to the
+% projects folder.
+
+addpath dataset
+addpath lib
+addpath src
