a76ae0fe082f01d5e97b074f7ec786c4566865cb
Piotr Pelczar <athlan@vgroup.pl>
2014-04-09 16:52:50 +0200

    Update createF.m

--- src/createF.m
+++ src/createF.m
@@ -1,4 +1,4 @@
-function [ F ] = createF( Q, windowSize_x, windowSize_y, fi )
+function [ F ] = createF( Q, windowSizeWidth, windowSizeHeight, fi )
 %CREATEF Create features F[1x16 x 9x9] matrixies
 %   Detailed explanation goes here
 
