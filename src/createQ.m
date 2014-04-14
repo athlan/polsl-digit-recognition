c4464cf62fc518d136b4764971ca89fb1192a2bb
Pawel Sikora <p.sikora@lgbs.pl>
2014-04-14 22:54:21 +0200

    DONE createQ

--- src/createQ.m
+++ src/createQ.m
@@ -7,5 +7,13 @@ function [ Q ] = createQ( I, G, radius )
     masksNumber = size(G,2);
     
     Q(1,masksNumber, size(G,3) - 2*radius,size(G,4) - 2*radius) = 0; % mock
+    L = G;
+    L(L<0)=0;
+    for j=1:size(G,2)
+        tempTable(size(G,3),size(G,3)) = 0;
+        tempTable(1:size(G,3),1:size(G,3)) = L(1:1,j,1:size(G,3),1:size(G,3));
+        tempTable = I*tempTable;
+        Q(1,j,1:size(G,3)-2*radius,1:size(G,3)-2*radius) = tempTable(1+ radius:size(tempTable,1)-radius,1+ radius:size(tempTable,1)-radius);
+    end;
 
 end
