61455ed5f8368fd63ba1dbe43064e4611901e039
Athlan <athlan@vgroup.pl>
2014-04-17 01:35:46 +0200

    createH Docs updated.

--- src/createH.m
+++ src/createH.m
@@ -1,5 +1,5 @@
 function [ H ] = createH( numberOfMasks, maskWidth, maskHeight )
-%CREATEH Create H[1x16 x 5x5] matricies
+%CREATEH Create H[16 x 5x5] matricies
 %   Detailed explanation goes here
 
     H(numberOfMasks, maskHeight,maskWidth) = 0; % mock
