276728facd6a3499a31143f5786eee5acab89a12
Pawel Sikora <p.sikora@lgbs.pl>
2014-04-27 11:59:32 +0200

    poprawa createQ

--- src/createQ.m
+++ src/createQ.m
@@ -4,14 +4,9 @@ function [ Q ] = createQ( I, G, radius )
 
     % G matrix should be truncated here by functon l()
     % 
-    
-    Q(size(G,1) - 2*radius,size(G,2) - 2*radius) = 0; % mock
     L = G;
     L(L<0)=0;
-    
-    tempTable(size(G,1),size(G,1)) = 0;
-    tempTable(1:size(G,1),1:size(G,1)) = L(1:size(G,1),1:size(G,1));
-    tempTable = I*tempTable;
-    Q(1:size(G,1)-2*radius,1:size(G,1)-2*radius) = tempTable(1+ radius:size(tempTable,1)-radius,1+ radius:size(tempTable,1)-radius);
+    tempTable = I.*L;
+    Q = tempTable(1+ radius:size(tempTable,1)-radius,1+ radius:size(tempTable,2)-radius);
 
 end
