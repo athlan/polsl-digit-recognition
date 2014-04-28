48d45ac15a9394e09e9da9bd253b78bd5c717d65
luki96 <fornal96@o2.pl>
2014-04-28 15:59:24 +0200

    Poprawa obliczania wspolczynnika W

--- src/createF.m
+++ src/createF.m
@@ -4,6 +4,7 @@ function [ F ] = createF( Q, v_x, v_y, u_x, u_y, fi, fillEmptyCellsValue)
 
     matrixF_x = floor((size(Q,1) - v_x) / u_x) + 1;
     matrixF_y = floor((size(Q,2) - v_y) / u_y) + 1;
+    previousValueOfY = -1;
     
     F(matrixF_x, matrixF_y) = 0;
     
@@ -13,7 +14,17 @@ function [ F ] = createF( Q, v_x, v_y, u_x, u_y, fi, fillEmptyCellsValue)
 
             for M = 0:v_x-1,
                 for N = 0:v_y-1,
-                     W = calculateW(M, N, X, Y, Q, v_x, v_y, u_x, u_y, fi);
+                     if (previousValueOfY ~= Y)
+                         denominator = 0;
+                         for S = 0:v_x-1,
+                             for T = 0:v_y-1,
+                                 denominator = denominator + (abs(Q(u_x * X + S + 1, u_y* Y + T + 1))) ^ fi;
+                             end
+                         end
+                         previousValueOfY = Y;
+                     end
+                     numerator =  (abs(Q(u_x * X + M + 1, u_y * Y + N + 1))) ^ fi;
+                     W = numerator / denominator;
                      tmp = tmp + (W * (Q(u_x * X + M + 1, u_y * Y + N + 1)));
                 end
             end
